d94ce915e8277647e70bf80198c12071603b26f4
ezkibel <ezequielsegundo7392@gmail.com>
2021-08-15 21:51:44 -0300

    nuevo cambios

--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,7 @@
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    var header = document.querySelector("header");
+    nav.classList.toggle("abajo2", window.scrollY>0);
+    header.classList.toggle("abajo", window.scrollY>0);
 
-let tarjetas = document.getElementById('tarjeta1', 'tarjeta2', 'tarjeta3', 'tarjeta4');
-
-let i = 0;
-
-const show = () => {
-    tarjetas = tarjetas(i);
-    i = (i < tarjetas.length -1) ? i+1:0;
-}
-
-const interval = () => {
-    setInterval(show,1000);
-}
+})
